f8f51b710ff53439d06fe886c3c7b4a3051b0d85
BerDuc <bernard_ducharme@yahoo.ca>
2020-07-13 12:28:43 -0400

    ajout d'un guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { IdentificationComponent } from './identification/identification.component';
-import { RechercheProComponent } from './recherche-pro/recherche-pro.component';
-import { ProfilComponent } from './profil/profil.component';
-import { CreationProfilComponent } from './creation-profil/creation-profil.component';
+import { IdentificationComponent } from './components/identification/identification.component';
+import { RechercheProComponent } from './components/recherche-pro/recherche-pro.component';
+import { ProfilComponent } from './components/profil/profil.component';
+import { CreationProfilComponent } from './components/creation-profil/creation-profil.component';
+import { AuthProGuardGuard } from './helpers/auth-pro-guard.guard';
 
 const routes: Routes = [
   { path: 'identification', component: IdentificationComponent },
   { path: 'cherchePro', component: RechercheProComponent },
-  { path: 'profil/:id', component: ProfilComponent},
+  { path: 'profil/:id', component: ProfilComponent, canActivate: [AuthProGuardGuard]},
   { path: 'creation-profil', component: CreationProfilComponent},
   { path: '', redirectTo: '/identification', pathMatch: 'full' }
 ];
