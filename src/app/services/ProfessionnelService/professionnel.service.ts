1eda14c23889a9034732083f79da65b688f6457d
BerDuc <bernard_ducharme@yahoo.ca>
2020-07-13 12:29:17 -0400

    correction de l'authentication

--- src/app/services/ProfessionnelService/professionnel.service.ts
+++ src/app/services/ProfessionnelService/professionnel.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Professionnel } from '../model/Professionnel';
-import { Observable, of } from 'rxjs';
+import { Professionnel } from '../../model/Professionnel';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { stringify } from 'querystring';
+import { environment } from '../../../environments/environment';
+import { map } from 'rxjs/operators';
+
+
+const proUrl = environment.base_url_Api+'Professionnels';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class ProfessionnelService {
 
-  private proUrl = 'https://localhost:44307/api/Professionnels';
+  private currentLoggedInProSubject: BehaviorSubject<Professionnel>;
+  public currentUser: Observable<Professionnel>;
   
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -19,35 +23,50 @@ export class ProfessionnelService {
   constructor(private http: HttpClient) { }
 
   getPro(id: number): Observable<Professionnel> {
-    return this.http.get<Professionnel>(this.proUrl+'/'+id);
+    return this.http.get<Professionnel>(proUrl+id);
   }
 
 
   getPros(): Observable<Professionnel[]> {
-    return this.http.get<Professionnel[]>(this.proUrl);
+    return this.http.get<Professionnel[]>(proUrl);
   }
 
-  validerPro(pro: Professionnel):Observable<Professionnel[]>{
-    //idéalement, ça devrait être GET, mais j'ai besoin d'un body, donc ce sera POST pour le moment
-    return this.http.post<Professionnel[]>(this.proUrl+"/Identification", 
-      {
-        courriel: pro.courriel,
-        mdp: pro.mdp
-      }, 
-      this.httpOptions); 
-  }  
+
+//login et logout
+public get currentProValue(): Professionnel{
+  return this.currentLoggedInProSubject.value;
+}
+
+validerPro(courriel: string, password: string):Observable<Professionnel> {
+  console.log('${environment.apiUrl}/api/logins');
+  const headers = new HttpHeaders({'Content-Type':'application/json',
+  'courriel':courriel,
+  'psw': password});
+  return this.http.get<Professionnel>(proUrl,{headers:headers})
+          .pipe(map(user =>{
+              // store the user details and jwt in local storage
+              localStorage.setItem('currentUser',JSON.stringify(user));
+              this.currentLoggedInProSubject.next(user);
+              return user;
+          }));  
+}
+
+logout(){
+    localStorage.removeItem('currentUser');
+    this.currentLoggedInProSubject.next(null);
+}
 
   searchPros(criteres: string):Observable<Professionnel[]> {
     console.log(criteres);
-    return this.http.get<Professionnel[]>(this.proUrl+"/Professionnels/expertise/"+criteres);
+    return this.http.get<Professionnel[]>(proUrl+"/Professionnels/expertise/"+criteres);
   }
 
   createPro(pro: Professionnel):Observable<Professionnel>{
-    return this.http.post<Professionnel>(this.proUrl, JSON.stringify(pro), this.httpOptions);
+    return this.http.post<Professionnel>(proUrl, JSON.stringify(pro), this.httpOptions);
   }
 
   modifPro(pro: Professionnel): Observable<any> {
-    var url = this.proUrl+"/"+pro.utilisateurID;
+    var url = proUrl+"/"+pro.utilisateurID;
     console.log(url);
     console.log(JSON.stringify(pro));
     return this.http.put(url, JSON.stringify(pro), this.httpOptions);
